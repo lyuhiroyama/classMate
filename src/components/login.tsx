b466b4da5b133c91006119822b4a79cb33735d2c
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-20 23:29:41 +0900

    feat: add login input fields

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,9 +1,14 @@
 export default function Login() {
     return (
         <div className="login-component">
-            <div className="login-box">
-                
+            <div className="login-container">
+                <div className="input-fields-container">
+                    <label>ログイン：</label>
+                    <input type="text" placeholder="ユーザーID" />
+                    <label>パスワード：</label>
+                    <input type="text" placeholder="パスワード" />
+                </div>
             </div>
         </div>
-    )
+    );
 }
