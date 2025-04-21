dccaa204b4e2e9b9034fc317773155810d5c41f1
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-21 23:21:47 +0900

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Login from './components/login'
+import Navbar from './components/navbar'
 
 function App() {
     return (
         <div className="app-component">
-            <Login />
+            <Navbar />
         </div>
     )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Navbar(){
+    return (
+        <div className="navbar-component">
+            <div className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>出席</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
