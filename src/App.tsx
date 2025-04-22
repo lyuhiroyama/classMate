837e6539e93243ff46f186f01af22328b1e54e93
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-23 00:15:29 +0900

    add: Home/Login/Contact components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.scss";
-import Login from './components/login'
-import Navbar from './components/navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
         <div className="app-component">
             <Navbar />
+            <Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+                <Route path="/contact" element={<Contact />} />
+			</Routes>
         </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,13 @@
-
+import { Link } from 'react-router-dom'
 
 export default function Navbar(){
     return (
         <div className="navbar-component">
             <div className="navbar">
                 <ul>
-                    <li>Home</li>
-                    <li>出席</li>
-                    <li>Contact</li>
+                    <li><Link to="/" className="nav-link">Home</Link></li>
+                    <li><Link to="/login" className="nav-link">ログイン</Link></li>
+                    <li><Link to="/contact" className="nav-link">Contact</Link></li>
                 </ul>
             </div>
         </div>
