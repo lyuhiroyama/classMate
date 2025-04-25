0b25248cf23caae7ba015514a2405c1f8e8faff5
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-25 23:44:00 +0900

    style: add styles to log-in box

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,10 +3,14 @@ export default function Login() {
         <div className="login-component">
             <div className="login-container">
                 <div className="input-fields-container">
-                    <label>ログイン：</label>
-                    <input type="text" placeholder="ユーザーID" />
-                    <label>パスワード：</label>
-                    <input type="text" placeholder="パスワード" />
+                    <div className="label-and-input">
+                        <label className="input-labels">ログイン：</label>
+                        <input type="text" placeholder="ユーザーID" />
+                    </div>
+                    <div className="label-and-input">
+                        <label className="input-labels">パスワード：</label>
+                        <input type="text" placeholder="パスワード" />
+                    </div>
                 </div>
             </div>
         </div>
