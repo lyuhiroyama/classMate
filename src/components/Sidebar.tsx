ead7ae52b1f7d4e75decbd692bdd3bd3715a80a3
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-27 22:32:11 +0900

    feat: add day-of-week drop-down menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,16 @@
 export default function Sidebar() {
     return (
         <div className="sidebar-component">
-            <h2 className="sidebar-title">
-                クラスリスト
-            </h2>
+            <h2 className="sidebar-title">クラスリスト</h2>
+            <select className="day-of-week-menu">
+                <option value="monday">月</option>
+                <option value="tuesday">火</option>
+                <option value="wednesday">水</option>
+                <option value="thursday">木</option>
+                <option value="friday">金</option>
+                <option value="saturday">土</option>
+                <option value="sunday">日</option>
+            </select>
         </div>
-    )
+    );
 }
