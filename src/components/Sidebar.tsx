a70c1701e944a0b59852e8737343d0c7ec9c83e7
lyuhiroyama <lyuhiroyama@gmail.com>
2025-05-02 10:45:49 +0900

    feat: Add days-of-week buttons to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,15 +2,15 @@ export default function Sidebar() {
     return (
         <div className="sidebar-component">
             <h2 className="sidebar-title">クラスリスト</h2>
-            <select className="day-of-week-menu">
-                <option value="monday">月</option>
-                <option value="tuesday">火</option>
-                <option value="wednesday">水</option>
-                <option value="thursday">木</option>
-                <option value="friday">金</option>
-                <option value="saturday">土</option>
-                <option value="sunday">日</option>
-            </select>
+            <div className="day-of-week-buttons">
+                <button>月</button>
+                <button>火</button>
+                <button>水</button>
+                <button>木</button>
+                <button>金</button>
+                <button>土</button>
+                <button>日</button>
+            </div>
         </div>
     );
 }
